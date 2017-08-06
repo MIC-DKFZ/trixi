52de05737a26cae1e152ed030d479a1871478906
david <d.zimmerer@gmail.com>
2017-08-06 22:09:36 +0200

    Added plotting of a pytorch model graph to the pytorch visdom logger

--- vislogger/pytorchvisdomlogger.py
+++ vislogger/pytorchvisdomlogger.py
@@ -1,5 +1,8 @@
+import tempfile
+
 import numpy as np
 import torch
+from graphviz import Digraph
 from torch.autograd import Variable
 
 from vislogger import NumpyVisdomLogger
@@ -103,3 +106,90 @@ class PytorchVisdomLogger(NumpyVisdomLogger):
         """
         for model_name, model in model_dict.items():
             self.plot_model_statistics_grads(model=model, env_app=env_app, model_name=model_name)
+
+    def plot_model_structure(self, model, input_size, name=None, use_cuda=True, delete_tmp_on_close=False):
+        """
+        Plots the model structure/ model graph of a pytorch module (this only works correctly with pytorch 0.2.0).
+
+        :param model: The graph of this model will be plotted
+        :param input_size: Input size of the model (with batch dim)
+        :param name: The name of the window in the visdom env
+        :param use_cuda: Perform model dimension calculations on the gpu (cuda)
+        :param delete_tmp_on_close: Determines if the tmp file will be deleted on close. If set true, can cause problems
+        due to the multi threadded plotting.
+        """
+
+        def make_dot(var, state_dict=None):
+            """ Produces Graphviz representation of PyTorch autograd graph
+            Blue nodes are the Variables that require grad, orange are Tensors
+            saved for backward in torch.autograd.Function
+            Args:
+                var: output Variable
+                state_dict: dict of (name, parameter) to add names to node that require grad
+            """
+            if state_dict is not None:
+                # assert isinstance(params.values()[0], Variable)
+                param_map = {id(v): k for k, v in state_dict.items()}
+
+            node_attr = dict(style='filled',
+                             shape='box',
+                             align='left',
+                             fontsize='12',
+                             ranksep='0.1',
+                             height='0.2')
+            dot = Digraph(node_attr=node_attr, graph_attr=dict(size="12,12"), format="svg")
+            seen = set()
+
+            def size_to_str(size):
+                return '(' + (', ').join(['%d' % v for v in size]) + ')'
+
+            def add_nodes(var):
+                if var not in seen:
+                    if torch.is_tensor(var):
+                        dot.node(str(id(var)), size_to_str(var.size()), fillcolor='orange')
+                    elif hasattr(var, 'variable'):
+                        u = var.variable
+                        if state_dict is not None and id(u.data) in param_map:
+                            name = param_map[id(u.data)]
+                        else:
+                            name = ""
+                        node_name = '%s\n %s' % (name, size_to_str(u.size()))
+                        dot.node(str(id(var)), node_name, fillcolor='lightblue')
+                    else:
+                        name = str(type(var).__name__)
+                        if name.endswith("Backward"):
+                            name = name[:-8]
+                        dot.node(str(id(var)), name)
+                    seen.add(var)
+                    if hasattr(var, 'next_functions'):
+                        for u in var.next_functions:
+                            if u[0] is not None:
+                                dot.edge(str(id(u[0])), str(id(var)))
+                                add_nodes(u[0])
+                    if hasattr(var, 'saved_tensors'):
+                        for t in var.saved_tensors:
+                            dot.edge(str(id(t)), str(id(var)))
+                            add_nodes(t)
+
+            add_nodes(var.grad_fn)
+            return dot
+
+        # Create input
+        inputs = Variable(torch.randn(input_size))
+        if use_cuda:
+            inputs = inputs.cuda()
+            model = model.cuda()
+
+        # get output
+        output = model(inputs)
+
+        # get temp file to store svg in
+        fp = tempfile.NamedTemporaryFile(suffix=".svg", delete=delete_tmp_on_close)
+
+        # Create model graph and store it as svg
+        g = make_dot(output, model.state_dict())
+        x = g.render(fp.name[:-4], cleanup=True)
+
+        # Display model graph in visdom
+        self.show_svg(svg=x, name=name)
+
