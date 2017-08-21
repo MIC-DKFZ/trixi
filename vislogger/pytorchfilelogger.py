c201b21e52a9d33cee37eadc4ebd4955a7ca2d97
david <d.zimmerer@dkfz-heidelberg.de>
2017-08-21 08:49:13 +0200

    Added model storing to pytorch file logger

--- vislogger/pytorchfilelogger.py
+++ vislogger/pytorchfilelogger.py
@@ -1,5 +1,11 @@
+import atexit
+
+import fnmatch
+import os
+
 import torch
 from torch.autograd import Variable
+from torchvision.utils import save_image
 
 from vislogger.numpyfilelogger import NumpyFileLogger
 
@@ -10,10 +16,8 @@ class PytorchFileLogger(NumpyFileLogger):
     file system.
     """
 
-
     def __init__(self, *args, **kwargs):
         super(PytorchFileLogger, self).__init__(*args, **kwargs)
-
 
     def process_params(self, f, *args, **kwargs):
         """
@@ -32,3 +36,151 @@ class PytorchFileLogger(NumpyFileLogger):
                 kwargs[key] = data.cpu().numpy()
 
         return f(self, *args, **kwargs)
+
+    def name_and_iter_to_filename(self, name, n_iter, ending, iter_format="%05d", prefix=False):
+        iter_str = iter_format % n_iter
+        if prefix:
+            name = iter_str + "_" + name + ending
+        else:
+            name = name + "_" + iter_str + ending
+
+        return name
+
+    def store_image(self, tensor, name, n_iter=None, prefix=False, iter_format="%05d", normalize=True):
+        """Stores an image"""
+
+        img_name = name
+
+        if n_iter is not None:
+            img_name = self.name_and_iter_to_filename(img_name, n_iter, ".png", iter_format=iter_format, prefix=prefix)
+
+        img_file = os.path.join(self.image_dir, img_name)
+        save_image(tensor, img_file, normalize=normalize)
+
+    def store_images(self, tensors, n_iter=None, prefix=False, iter_format="%05d", normalize=True):
+        assert isinstance(tensors, dict)
+
+        for name, tensor in tensors.items():
+            self.store_image(tensor=tensor, name=name, n_iter=n_iter, prefix=prefix, iter_format=iter_format,
+                             normalize=normalize)
+
+    def store_model(self, model, name, n_iter=None, prefix=False, iter_format="%05d"):
+        """Stores a model"""
+
+        model_name = name
+
+        if n_iter is not None:
+            model_name = self.name_and_iter_to_filename(model_name, n_iter, ".pth", iter_format=iter_format,
+                                                        prefix=prefix)
+
+        model_file = os.path.join(self.model_dir, model_name)
+        torch.save(model.state_dict(), model_file)
+
+    def store_models(self, models, n_iter=None, prefix=False, iter_format="%05d"):
+        assert isinstance(models, dict)
+
+        for name, model in models.items():
+            self.store_model(model=model, name=name, n_iter=n_iter, prefix=prefix, iter_format=iter_format)
+
+    def load_model(self, model, model_file, exclude_layers=None):
+        if os.path.exists(model_file):
+
+            if exclude_layers is None:
+                exclude_layers = []
+
+            # also allow loading of partially pretrained net
+            pretrained_dict = torch.load(model_file)
+            model_dict = model.state_dict()
+
+            # 1. filter out unnecessary keys
+            pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict and k not in exclude_layers}
+            # 2. overwrite entries in the existing state dict
+            model_dict.update(pretrained_dict)
+            # 3. load the new state dict
+            model.load_state_dict(model_dict)
+
+        else:
+            raise ValueError("Model file does not exisit !")
+
+    def load_model_from_dir(self, model, dir, name, n_iter=None, prefix=False, iter_format="%05d", exclude_layers=None):
+
+        # If not iteration given, take the most recent model
+        if n_iter is None:
+
+            model_files = []
+            match_name = "*" + name + "*.pth"
+
+            for root, dirs, files in os.walk(dir):
+                for filename in fnmatch.filter(files, match_name):
+                    model_file = os.path.join(root, filename)
+                    model_files.append(model_file)
+
+            model_name = sorted(model_files, reverse=True)[1]
+
+        else:
+            model_name = self.name_and_iter_to_filename(name, n_iter, ".pth", iter_format=iter_format, prefix=prefix)
+
+        model_file = os.path.join(dir, model_name)
+        self.load_model(model, model_file, exclude_layers=exclude_layers)
+
+    def load_models_from_dir(self, models, dir, n_iter=None, prefix=False, iter_format="%05d", step_size=25,
+                             exclude_layers={}):
+        assert isinstance(models, dict) and isinstance(exclude_layers, dict)
+
+        for name, model in models.items():
+            self.load_model_from_dir(model, dir, name, n_iter=n_iter, prefix=prefix, iter_format=iter_format,
+                                     step_size=step_size, exclude_layers=exclude_layers.get(name))
+
+    def store_checkpoint(self, name, **kwargs):
+        for key, value in kwargs.items():
+            if isinstance(value, torch.nn.Module) or isinstance(value, torch.optim.Optimizer):
+                kwargs[key] = value.state_dict()
+
+        checkpoint_file = os.path.join(self.model_dir, name)
+
+        torch.save(kwargs, checkpoint_file)
+
+    def restore_checkpoint(self, name, **kwargs):
+        checkpoint = torch.load(name)
+
+        for key, value in kwargs.items():
+            if key in checkpoint:
+                if isinstance(value, torch.nn.Module) or isinstance(value, torch.optim.Optimizer):
+                    kwargs[key] = value.load_state_dict(checkpoint[key])
+                else:
+                    kwargs[key] = checkpoint[key]
+
+        return kwargs
+
+    def save_at_exit(self, **kwargs):
+        filename = "checkpoint_end.pth.tar"
+
+        def save_fnc():
+            self.store_checkpoint(filename, **kwargs)
+            print("Checkpoint stored securely... =)")
+
+        atexit.register(save_fnc)
+
+    def get_store_checkpoint_fn(self, **kwargs):
+        def save_fnc(n_iter, iter_format="%05d", prefix=False):
+            name = self.name_and_iter_to_filename(name="checkpoint", n_iter=n_iter, ending=".pth.tar",
+                                                  iter_format=iter_format,
+                                                  prefix=prefix)
+            self.store_checkpoint(name, **kwargs)
+
+        return save_fnc
+
+    def restore_lastest_checkpoint(self, dir, name=None, **kwargs):
+        if name is None:
+            name = "*checkpoint*.pth.tar"
+
+        checkpoint_files = []
+
+        for root, dirs, files in os.walk(dir):
+            for filename in fnmatch.filter(files, name):
+                checkpoint_file = os.path.join(root, filename)
+                checkpoint_files.append(checkpoint_file)
+
+        lastest_file = sorted(checkpoint_files, reverse=True)[1]
+
+        return self.restore_checkpoint(lastest_file, **kwargs)
