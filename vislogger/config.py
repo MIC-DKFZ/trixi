5d12861d48962bbaa52cbd7cb11f88f4b6e48b18
Jens Petersen <jens.petersen@dkfz.de>
2017-11-26 17:59:25 +0100

    Config can now be serialized with types

--- vislogger/config.py
+++ vislogger/config.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import importlib
 import json
+import re
 
 class Config(dict):
 
@@ -17,11 +19,23 @@ class Config(dict):
 
     def dump(self, file_, *args, **kwargs):
 
+        dump_data = self.copy()
+
+        for key, val in dump_data.items():
+            if type(val) == type:
+                try:
+                    module_ = val.__module__
+                    name_ = val.__name__
+                    repr_ = "type({}.{})".format(module_, name_)
+                    dump_data[key] = repr_
+                except Exception as e:
+                    raise e
+
         if hasattr(file_, "write"):
-            json.dump(self, file_, *args, **kwargs)
+            json.dump(dump_data, file_, *args, **kwargs)
         else:
             with open(file_, "w") as file_object:
-                json.dump(self, file_object, *args, **kwargs)
+                json.dump(dump_data, file_object, *args, **kwargs)
 
     def load(self, file_, *args, **kwargs):
 
@@ -30,5 +44,20 @@ class Config(dict):
         else:
             with open(file_, "r") as file_object:
                 new_dict = json.load(file_object, *args, **kwargs)
+
+        for key, val in new_dict.items():
+            if isinstance(val, str):
+                print("Check1")
+                if re.match("type\(.+\)", val):
+                    print("Check2")
+                    try:
+                        str_ = val[5:-1]
+                        module_ = ".".join(str_.split(".")[:-1])
+                        name_ = str_.split(".")[-1]
+                        print("Check3", module_, name_)
+                        type_ = getattr(importlib.import_module(module_), name_)
+                        new_dict[key] = type_
+                    except Exception as e:
+                        raise e
 
         self.update(new_dict)
