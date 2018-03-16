14509d816de631152515d1df69f3e868cc97497a
Jens Petersen <jens.petersen@dkfz.de>
2018-03-16 17:59:53 +0100

    Can now plot exp results with smoothing

--- vislogger/experiment_browser/browser.py
+++ vislogger/experiment_browser/browser.py
@@ -1,8 +1,12 @@
 import argparse
 import json
+import numpy as np
 import os
+from scipy.ndimage.filters import gaussian_filter1d
 
-from flask import Flask, render_template, request, Blueprint
+from flask import Flask, render_template, request, Blueprint, Markup
+from plotly.offline import plot
+import plotly.graph_objs as go
 
 from .experiment import Experiment
 
@@ -42,7 +46,7 @@ def process_base_dir(base_dir):
         if os.path.isdir(dir_path):
 
             exp = Experiment(dir_path)
-            keys.update(list(exp.keys()))
+            keys.update(list(exp.config.keys()))
             exps.append(exp)
 
     ### Remove unwanted keys
@@ -55,12 +59,12 @@ def process_base_dir(base_dir):
     for exp in exps:
         sub_row = []
         for key in sorted_keys:
-            attr_strng = str(getattr(exp, key, "----"))
+            attr_strng = str(getattr(exp.config, key, "----"))
             sub_row.append((attr_strng, attr_strng[:25]))
         rows.append((os.path.basename(exp.work_dir),
-                    str(getattr(exp, "experiment_name", "----")),
-                    str(getattr(exp, "init_time", "----")),
-                    str(getattr(exp, "", "----")),
+                    str(getattr(exp.config, "experiment_name", "----")),
+                    str(getattr(exp.config, "init_time", "----")),
+                    str(getattr(exp.config, "note", "----")),
                     sub_row))
 
 
@@ -71,31 +75,65 @@ def get_experiment_content(experiment_dir):
 
     exp = Experiment(experiment_dir)
 
-    images = exp.get_images()
-    plots = exp.get_plots()
-    models = exp.get_checkpoints()
+    #images = exp.get_images()
+    #plots = exp.get_plots()
+    #models = exp.get_checkpoints()
 
-    return {'images': images, 'plots': plots, 'models': models}
+    results, spacings = exp.get_results()
+
+    graphs = []
+    for key in results:
+        if np.issubdtype(results[key].dtype, np.number):
+            graphs.append(make_graph(key, results[key], spacings[key], smoothing=100.))
+
+    return {"graphs": graphs}
+
+    #return {'images': images, 'plots': plots, 'models': models}
+
+def make_graph(name, y, x_scale=1., trace_options=None, smoothing=False):
+
+    traces = []
+    x = np.arange(1, y.shape[0]+1) * x_scale
+    if trace_options is None: trace_options = {}
+
+    if smoothing:
+        filter_ = lambda x: gaussian_filter1d(x, smoothing)
+    else:
+        filter_ = lambda x: x
+
+    if y.ndim == 1:
+        traces.append(go.Scatter(x=x, y=filter_(y), **trace_options))
+
+    elif y.ndim == 2:
+        for t in range(y.shape[1]):
+            traces.append(go.Scatter(x=x, y=filter_(y[:,t]), **trace_options))
+
+    else:
+        print("ERR: can only create plots for arrays with ndim <= 3.")
+        return ""
+
+    layout = go.Layout(title=name)
+    markup = plot({"data": traces, "layout": layout},
+                  output_type="div",
+                  include_plotlyjs=False,
+                  show_link=False)
+
+    return Markup(markup)
 
 @app.route("/")
 def overview():
     base_info = process_base_dir(base_dir)
     return render_template("overview.html", title=base_dir, **base_info)
 
-@app.route('/experiment/', methods=["GET"])
-def experiment():
-
-    id_ = request.args.get('id')
-    if id_ is None:
-        return "ERR: Please give an experiment ID"
-
-    experiment_dir = os.path.join(base_dir, id_)
+@app.route('/experiment/<experiment_name>', methods=['GET'])
+def experiment(experiment_name):
+    experiment_dir = os.path.join(base_dir, experiment_name)
 
     if not os.path.exists(experiment_dir) or not os.path.isdir(experiment_dir):
         return "ERR: Please give a valid experiment directory"
 
     experiment_content = get_experiment_content(experiment_dir)
-    return render_template('experiment.html', title=id_, **experiment_content)
+    return render_template('experiment.html', title=experiment_name, **experiment_content)
 
 if __name__ == "__main__":
     app.run()
--- vislogger/experiment_browser/experiment.py
+++ vislogger/experiment_browser/experiment.py
@@ -1,7 +1,15 @@
+import json
+import numpy as np
 import os
+import re
+from itertools import tee, filterfalse
 from vislogger import Config
 
-class Experiment(Config):
+def partition(pred, iterable):
+    t1, t2 = tee(iterable)
+    return filter(pred, t1), filterfalse(pred, t2)
+
+class Experiment(object):
 
     def __init__(self, work_dir, *args, **kwargs):
 
@@ -16,7 +24,8 @@ class Experiment(Config):
         self.save_dir = os.path.join(self.work_dir, "save")
         self.result_dir = os.path.join(self.work_dir, "result")
 
-        self.load(os.path.join(self.config_dir, "config.json"))
+        self.config = Config()
+        self.config.load(os.path.join(self.config_dir, "config.json"))
 
     def get_file_contents(self, folder):
 
@@ -37,3 +46,37 @@ class Experiment(Config):
 
     def get_logs(self):
         return self.get_file_contents(self.log_dir)
+
+    def get_results(self):
+
+        try:
+            n_digits = "{" + str(len(str(self.config.num_epochs))) + "}"
+        except:
+            n_digits = "+"
+        epoch_pattern = ".*\d" + n_digits + ".*\.json"
+
+        result_files = filter(lambda x: re.search(epoch_pattern, x), sorted(os.listdir(self.result_dir)))
+        results = {}
+        epoch_spacings = {}
+
+        def update_results(new_dict, results_dict, spacings_dict, counter):
+            for k in new_dict.keys():
+                if isinstance(new_dict[k], dict):
+                    flat = {"{}.{}".format(k, key): val for key, val in new_dict[k].items()}
+                    update_results(flat, results_dict, spacings_dict, counter)
+                else:
+                    if k not in results_dict.keys():
+                        results_dict[k] = [new_dict[k]]
+                        spacings_dict[k] = counter
+                    else:
+                        results[k].append(new_dict[k])
+
+        for f, file_ in enumerate(result_files):
+            with open(os.path.join(self.result_dir, file_), "r") as infile:
+                current = json.load(infile)
+                update_results(current, results, epoch_spacings, f+1)
+
+        for k in results.keys():
+            results[k] = np.array(results[k])
+
+        return results, epoch_spacings
