9fde6c9113265347690be115a1ff1c68db651fe1
David Zimmerer <d.zimmerer@dkfz-heidelberg.de>
2018-04-27 07:54:38 +0200

    Add Error backprop onto the image for visualization to pytorchvisdom logger

--- vislogger/pytorchvisdomlogger.py
+++ vislogger/pytorchvisdomlogger.py
@@ -1,5 +1,6 @@
 import atexit
 import tempfile
+import warnings
 from multiprocessing import Process
 
 import numpy as np
@@ -314,18 +315,15 @@ class PytorchVisdomLogger(NumpyVisdomLogger):
         atexit.register(p.terminate)
         p.start()
 
-
     @convert_params
     def show_roc_curve(self, tensor, labels, name):
 
         from sklearn import metrics
 
         def __show_roc_curve(self, tensor, labels, name):
-
             fpr, tpr, thresholds = metrics.roc_curve(labels.flatten(), tensor.flatten())
             self.show_lineplot(tpr, fpr, name=name, opts={"fillarea": True})
             # self.add_to_graph(x_vals=np.arange(0, 1.1, 0.1), y_vals=np.arange(0, 1.1, 0.1), name=name, append=True)
-
 
         p = Process(target=__show_roc_curve, kwargs=dict(self=self,
                                                          tensor=tensor,
@@ -345,7 +343,6 @@ class PytorchVisdomLogger(NumpyVisdomLogger):
             self.show_lineplot(precision, recall, name=name, opts={"fillarea": True})
             # self.add_to_graph(x_vals=np.arange(0, 1.1, 0.1), y_vals=np.arange(0, 1.1, 0.1), name=name, append=True)
 
-
         p = Process(target=__show_roc_curve, kwargs=dict(self=self,
                                                          tensor=tensor,
                                                          labels=labels,
@@ -353,7 +350,6 @@ class PytorchVisdomLogger(NumpyVisdomLogger):
                                                          ))
         atexit.register(p.terminate)
         p.start()
-
 
     @convert_params
     def show_classification_metrics(self, tensor, labels, name, metric=("roc-auc", "pr-auc", "pr-score")):
@@ -391,3 +387,82 @@ class PytorchVisdomLogger(NumpyVisdomLogger):
                                                          ))
         atexit.register(p.terminate)
         p.start()
+
+    def get_vanilla_image_gradient(self, model, inpt, err_fn):
+
+        if isinstance(model, torch.nn.Module):
+            model.zero_grad()
+        inpt = inpt.detach()
+        inpt.requires_grad = True
+
+        output = model(inpt)
+
+        err = err_fn(output)
+        err.backward()
+
+        grad = inpt.grad + 0
+
+        if isinstance(model, torch.nn.Module):
+            model.zero_grad()
+        return grad
+
+    def get_guided_image_gradient(self, model: torch.nn.Module, inpt, err_fn):
+
+        def guided_relu_hook_function(module, grad_in, grad_out):
+            if isinstance(module, (torch.nn.ReLU, torch.nn.LeakyReLU)):
+                return (torch.clamp(grad_in[0], min=0.0),)
+
+        model.zero_grad()
+
+        ### Apply hooks
+        hook_ids = []
+        for mod in model.modules():
+            hook_id = mod.register_backward_hook(guided_relu_hook_function)
+            hook_ids.append(hook_id)
+
+        inpt = inpt.detach()
+        inpt.requires_grad = True
+
+        output = model(inpt)
+
+        err = err_fn(output)
+        err.backward()
+
+        grad = inpt.grad + 0
+
+        model.zero_grad()
+        for hooks in hook_ids:
+            hooks.remove()
+
+        return grad
+
+    def get_smooth_image_gradient(self, model, inpt, err_fn, n_runs=20, eps=0.1, grad_type="vanilla"):
+        grads = []
+        for i in range(n_runs):
+            inpt = inpt + torch.randn(inpt.size()).to(inpt.device) * eps
+            if grad_type == "vanilla":
+                single_grad = self.get_vanilla_image_gradient(model, inpt, err_fn)
+            elif grad_type == "guided":
+                single_grad = self.get_guided_image_gradient(model, inpt, err_fn)
+            else:
+                warnings.warn("This grad_type is not implemented yet")
+                single_grad = torch.zeros_like(inpt)
+            grads.append(single_grad)
+
+        grad = torch.mean(torch.stack(grads), dim=0)
+        return grad
+
+    def show_image_gradient(self, model, inpt, err_fn, grad_type="vanilla", n_runs=20, eps=0.1, **image_grid_params):
+        if grad_type == "vanilla":
+            grad = self.get_vanilla_image_gradient(model, inpt, err_fn)
+        elif grad_type == "guided":
+            grad = self.get_guided_image_gradient(model, inpt, err_fn)
+        elif grad_type == "smooth-vanilla":
+            grad = self.get_smooth_image_gradient(model, inpt, err_fn, n_runs, eps, grad_type="vanilla")
+        elif grad_type == "smooth-guided":
+            grad = self.get_smooth_image_gradient(model, inpt, err_fn, n_runs, eps, grad_type="guided")
+        else:
+            warnings.warn("This grad_type is not implemented yet")
+            grad = torch.zeros_like(inpt)
+
+        self.show_image_grid(grad, **image_grid_params)
