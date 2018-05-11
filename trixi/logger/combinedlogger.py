3d094e8c3b6aa83ace80a5512284086a3b2acb5d
David Zimmerer <d.zimmerer@dkfz-heidelberg.de>
2018-05-11 16:32:56 +0200

    Document CombinedLogger

--- trixi/logger/combinedlogger.py
+++ trixi/logger/combinedlogger.py
@@ -46,7 +46,32 @@ def create_function(self, sub_methods):
 
 
 class CombinedLogger(object):
+    """
+    A Logger which can combine all other logger and if called calls all the sub loggers
+
+    """
+
     def __init__(self, *loggers):
+        """
+        Initializes a new combined logger with a list of logger and their logging frequencies.
+
+        By default it initalized a counter for each logger method, and on each call it increases the counter (and if the
+        counter is a multiple of the frequency it logs to the given logger).
+
+        Furthermore you can use the same_as_last argument in all methods to repeat the frequency_counter from the
+        previous call. If you set the do_not_increase attribute to True, it wont increase the counter, it has been
+        called 10 times and the frequency is 10, it will log, and also log in the next round, since the counter is
+        still 10 and was not increase. Use can use the log_all attribute, it you want to ignore the frequencies and
+        simply log to all loggers. By default, if your method has a name attribute, there is a counter for each
+        method called with a unique name (so one counter for plot(name="1") and a different counter for plot(name="2")),
+        but if you want one counter for the method (despite having a name attribute), you can set the
+        ignore_name_in_args to True,
+
+        Args:
+            *loggers (list): a list of tuples where each tuple conisist of (logger, frequencies), where logger is
+            a given logger and frequencies is the frequency (or rather log each on in frequencies, e.g. if it is 10,
+            it logs every ten_th call).
+        """
 
         self.loggers, self.frequencies = zip(*loggers)
 
