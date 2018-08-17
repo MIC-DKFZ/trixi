39a6fc14f60a37aac4e5a102ef4401ede1acc724
Jens Petersen <jens.petersen@dkfz.de>
2018-08-17 14:21:55 +0200

    Mock portalocker, torch and seaborn for autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -176,7 +176,7 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-autodoc_mock_imports = ["matplotlib", "numpy", "scipy"]
+autodoc_mock_imports = ["matplotlib", "numpy", "scipy", "portalocker", "torch", "seaborn"]
 
 
 def run_apidoc(_):
