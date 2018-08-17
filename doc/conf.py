0bcea5b14d7480fb3b6c5596d7c1909e347f8b2f
Jens Petersen <jens.petersen@dkfz.de>
2018-08-17 14:27:36 +0200

    Mock torchvision for autodoc

--- doc/conf.py
+++ doc/conf.py
@@ -176,7 +176,16 @@ texinfo_documents = [
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
-autodoc_mock_imports = ["matplotlib", "numpy", "scipy", "portalocker", "torch", "seaborn", "visdom"]
+autodoc_mock_imports = [
+    "matplotlib",
+    "numpy",
+    "scipy",
+    "portalocker",
+    "torch",
+    "torchvision",
+    "seaborn",
+    "visdom"
+]
 
 
 def run_apidoc(_):
