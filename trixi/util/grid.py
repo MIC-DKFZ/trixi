43c7bf9b3eaaf9241907afbc57d4b9d623de7ef6
Jens Petersen <jens.petersen@dkfz.de>
2018-08-20 16:23:22 +0200

    Implement simple grid search

---
+++ trixi/util/grid.py
@@ -0,0 +1,36 @@
+
+
+class GridSearch(dict):
+
+    def all_combinations(self):
+
+        combinations = []
+        zero_key = sorted(self.keys())[0]
+        inner_dict = self.copy()
+        del inner_dict[zero_key]
+
+        for val in self[zero_key]:
+
+            if len(inner_dict) == 0:
+                combinations.append({zero_key: val})
+            else:
+                for combination in GridSearch(inner_dict):
+                    combination[zero_key] = val
+                    combinations.append(combination)
+
+        return combinations
+
+    def __iter__(self):
+
+        self._counter = 0
+        self._all_combinations = self.all_combinations()
+        self._max = len(self._all_combinations)
+        return self
+
+    def __next__(self):
+
+        if self._counter >= self._max:
+            raise StopIteration
+
+        self._counter += 1
+        return self._all_combinations[self._counter - 1]
