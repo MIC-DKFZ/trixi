345e25e9594d6fcf0fe1b55c4f640106437467de
Jens Petersen <jens.petersen@dkfz.de>
2018-11-23 17:23:38 +0100

    Make version accessible from within python

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from setuptools import setup
 
@@ -15,17 +16,28 @@ def resolve_requirements(file):
     return requirements
 
 
-with open(os.path.join(os.path.dirname(__file__), "Readme.md")) as f:
-    readme = f.read()
+def read_file(file):
+    with open(file) as f:
+        content = f.read()
+    return content
 
-with open(os.path.join(os.path.dirname(__file__), "LICENSE")) as f:
-    license = f.read()
+
+def find_version(file):
+    content = read_file(file)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 required = resolve_requirements(os.path.join(os.path.dirname(__file__), 'requirements_full.txt'))
+readme = read_file(os.path.join(os.path.dirname(__file__), "Readme.md"))
+license = read_file(os.path.join(os.path.dirname(__file__), "LICENSE"))
+version = find_version(os.path.join(os.path.dirname(__file__), "trixi", "__init__.py"))
 
 setup(name='trixi',
-      version='0.1.0.1',
-      description='Package to log visualizations and experiments, e.g. with visdom',
+      version=version,
+      description='Manage your machine learning experiments with trixi - modular, reproducible, high fashion',
       long_description=readme,
       long_description_content_type="text/markdown",
       url='https://github.com/MIC-DKFZ/trixi',
--- trixi/__init__.py
+++ trixi/__init__.py
@@ -1,3 +1,5 @@
 use_agg = True
 import matplotlib
 if use_agg: matplotlib.use("Agg", warn=False)
+
+__version__ = "0.1.1.0"
