1ebe0c84173b675b510ecca7518d05d413aa6510
Klaus Kades <k.kades@dkfz-heidelberg.de>
2019-02-01 16:25:10 +0100

    added pipeline to backend - axios

--- trixi/experiment_browser/frontend/app/containers/OverviewPage/OverviewPage.js
+++ trixi/experiment_browser/frontend/app/containers/OverviewPage/OverviewPage.js
@@ -5,6 +5,7 @@
  */
 
 import React from 'react';
+import axios from 'axios';
 import BootstrapTable from 'react-bootstrap-table-next';
 import Button from 'react-bootstrap/lib/Button';
 import Modal from 'react-bootstrap/lib/Modal';
@@ -12,7 +13,6 @@ import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import './style.scss';
-
 
 export default class OverviewPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   /**
@@ -30,6 +30,16 @@ export default class OverviewPage extends React.PureComponent { // eslint-disabl
     };
   }
 
+  // docu : https://alligator.io/react/axios-react/
+  componentDidMount() {
+    axios.get('http://localhost:5000/get_overview?dir=test_klaus')
+      .then((res) => {
+        console.log(res);
+        // const persons = res.data;
+        // this.setState({ persons });
+      });
+  }
+
   handleClose() {
     this.setState({ show: false });
   }
@@ -38,8 +48,11 @@ export default class OverviewPage extends React.PureComponent { // eslint-disabl
     this.setState({ show: true });
   }
 
-  render() {     
-    const baseInfo = { ccols1: ['another_entry', 'batch_size', 'num_epochs'], ccols2: ['description', 'patch_size'], rcols: ['auc', 'dice', 'train', 'val'], rows: [['experiment_browser/experiment1', false, 'experiments', '20180221-163831', 'Finished', '-', [['-', '-'], ['2', '2'], ['1000', '1000'], ['This is only to test the experiment browser', 'This is only to test the '], ['(128, 128, 128)', '(128, 128, 128)']], [['0.07968243956565857', '0.07968243956565857'], ['0.22086010873317719', '0.22086010873317719'], ['0.051016926765441895', '0.051016926765441895'], ['0.35155946016311646', '0.35155946016311646']]], ['experiment_browser/experiment2', false, 'experiments', '20180221-163831', 'Error', '-', [['something something', 'something something'], ['3', '3'], ['2000', '2000'], ['This is only to test the experiment browser', 'This is only to test the '], ['(128, 128, 128)', '(128, 128, 128)']], [['0.07968243956565857', '0.07968243956565857'], ['0.22086010873317719', '0.22086010873317719'], ['0.051016926765441895', '0.051016926765441895'], ['0.35155946016311646', '0.35155946016311646']]]], noexp: [] }
+
+  render() {
+    const baseInfo = {
+      ccols1: ['another_entry', 'batch_size', 'num_epochs'], ccols2: ['description', 'patch_size'], rcols: ['auc', 'dice', 'train', 'val'], rows: [['experiment_browser/experiment1', false, 'experiments', '20180221-163831', 'Finished', '-', [['-', '-'], ['2', '2'], ['1000', '1000'], ['This is only to test the experiment browser', 'This is only to test the '], ['(128, 128, 128)', '(128, 128, 128)']], [['0.07968243956565857', '0.07968243956565857'], ['0.22086010873317719', '0.22086010873317719'], ['0.051016926765441895', '0.051016926765441895'], ['0.35155946016311646', '0.35155946016311646']]], ['experiment_browser/experiment2', false, 'experiments', '20180221-163831', 'Error', '-', [['something something', 'something something'], ['3', '3'], ['2000', '2000'], ['This is only to test the experiment browser', 'This is only to test the '], ['(128, 128, 128)', '(128, 128, 128)']], [['0.07968243956565857', '0.07968243956565857'], ['0.22086010873317719', '0.22086010873317719'], ['0.051016926765441895', '0.051016926765441895'], ['0.35155946016311646', '0.35155946016311646']]]], noexp: []
+    };
 
     const products = [{
       directory: '1',
@@ -106,21 +119,17 @@ export default class OverviewPage extends React.PureComponent { // eslint-disabl
 
             <BootstrapTable keyField="idasdf" data={products} columns={columns} filter={filterFactory()} />
 
-            <div className="container-fluid">
+            {/* <div className="container-fluid">
               <div className="row text-left">
                 <div className="col-auto" style={{ paddingRight: '2px' }}>
                   <a role="button" className="btn btn-outline-secondary toggle-config">Toggle all</a>
                 </div>
                 <div className="col" style={buttonPaddingStyle}>
                   <div>
-                    {baseInfo.ccols1.map((name) => {
-                      return <Button key={name} variant="outline-success">{name}</Button>;
-                    })}
+                    {baseInfo.ccols1.map((name) => <Button key={name} variant="outline-success">{name}</Button>)}
                   </div>
                   <div>
-                    {baseInfo.ccols2.map((name) => {
-                      return <Button key={name} variant="outline-success">{name}</Button>;
-                    })}
+                    {baseInfo.ccols2.map((name) => <Button key={name} variant="outline-success">{name}</Button>)}
                   </div>
                 </div>
               </div>
@@ -129,9 +138,7 @@ export default class OverviewPage extends React.PureComponent { // eslint-disabl
                   <a role="button" className="btn btn-outline-secondary toggle-config">Toggle all</a>
                 </div>
                 <div className="col" style={buttonPaddingStyle}>
-                  {baseInfo.rcols.map((name) => {
-                    return <Button key={name} variant="outline-success">{name}</Button>;
-                  })}
+                  {baseInfo.rcols.map((name) => <Button key={name} variant="outline-success">{name}</Button>)}
                 </div>
               </div>
               <div className="row" style={{ marginTop: '5px' }}>
@@ -141,7 +148,7 @@ export default class OverviewPage extends React.PureComponent { // eslint-disabl
                   </button>
                 </div>
               </div>
-            </div>
+            </div> */}
           </section>
           <section>
             <Modal show={this.state.show} onHide={this.handleClose}>
