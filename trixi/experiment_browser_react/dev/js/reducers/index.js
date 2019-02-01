f52cbbfe877e4d8f192f8421b51262a45708a654
Tobias Ross <t.ross@dkfz-heidelberg.de>
2019-02-01 11:17:19 +0100

    rearranged folder structure

--- trixi/experiment_browser_react/dev/js/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-import DummyReducer from './reducer-dummy'
-
-const allReducers=combineReducers({
-    dummy: DummyReducer,
-})
-export default allReducers
--- trixi/experiment_browser_react/dev/js/reducers/reducer-dummy.js
+++
@@ -1,4 +0,0 @@
-export default function (state = null, action) {
-    state = 0
-    return state
-}
