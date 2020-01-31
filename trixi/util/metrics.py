0dffd6b2590c40cf18d65c3ba4fb7a4ca2d418b6
David Zimmerer <d.zimmerer@dkfz-heidelberg.de>
2020-01-31 11:17:35 +0100

    Changed return type from get metrics to dict instead of (ordered) list

--- trixi/util/metrics.py
+++ trixi/util/metrics.py
@@ -1,8 +1,10 @@
 import atexit
 import warnings
+from collections import OrderedDict
 from multiprocessing import Process
 
 import numpy as np
+from sklearn import metrics
 
 
 def get_roc_curve(tensor, labels, reduce_to_n_samples=None, use_sub_process=False, results_fn=lambda
@@ -18,7 +20,6 @@ def get_roc_curve(tensor, labels, reduce_to_n_samples=None, use_sub_process=Fals
         results_fn: function which is called with the results/ return values. Expected f(tpr, fpr)
 
     """
-    from sklearn import metrics
 
     def __get_roc_curve(tensor, labels, reduce_to_n_samples=None, results_fn=lambda x, *y, **z: None):
 
@@ -69,7 +70,6 @@ def get_pr_curve(tensor, labels, reduce_to_n_samples=None, use_sub_process=False
         results_fn: function which is called with the results/ return values. Expected f(precision, recall)
 
     """
-    from sklearn import metrics
 
     def __get_pr_curve(tensor, labels, reduce_to_n_samples=None, results_fn=lambda x, *y, **z: None):
 
@@ -107,6 +107,41 @@ def get_pr_curve(tensor, labels, reduce_to_n_samples=None, use_sub_process=False
             warnings.warn("Sth went wrong with calculating the pr curve")
 
 
+def _get_classification_metrics(tensor, labels, metric="roc-auc"):
+    """
+   Calculates a metric given the predicted values and the given/correct labels.
+
+    Args:
+        tensor: Tensor with scores (e.g class probability )
+        labels: Labels of the samples to which the scores match
+        metric: List of metrics to calculate. Options are: roc-auc, pr-auc, pr-score, mcc, f1
+
+    Returns:
+        The metric value
+
+    """
+
+    if not isinstance(labels, list):
+        labels = labels.flatten()
+    if not isinstance(tensor, list):
+        tensor = tensor.flatten()
+
+    metric_value = 0.0
+    if "roc-auc" == metric:
+        metric_value = metrics.roc_auc_score(labels, tensor)
+    if "pr-auc" == metric:
+        precision, recall, thresholds = metrics.precision_recall_curve(labels, tensor)
+        metric_value = metrics.auc(recall, precision)
+    if "pr-score" == metric:
+        metric_value = metrics.average_precision_score(labels, tensor)
+    if "mcc" == metric:
+        metric_value = metrics.matthews_corrcoef(labels, tensor)
+    if "f1" == metric:
+        metric_value = metrics.f1_score(labels, tensor)
+
+    return metric_value
+
+
 def get_classification_metrics(tensor, labels, name="", metric=("roc-auc", "pr-score"), use_sub_process=False,
                                tag_name=None, results_fn=lambda x, *y, **z: None):
     """
@@ -126,45 +161,23 @@ def get_classification_metrics(tensor, labels, name="", metric=("roc-auc", "pr-s
 
     """
 
-    from sklearn import metrics
-
     def __get_classification_metrics(tensor, labels, name="", metric=("roc-auc", "pr-score"),
                                      tag_name=None, results_fn=lambda x, *y, **z: None):
-
-        vals = []
-        tags = []
 
         if not isinstance(labels, list):
             labels = labels.flatten()
         if not isinstance(tensor, list):
             tensor = tensor.flatten()
 
-        if "roc-auc" in metric:
-            roc_auc = metrics.roc_auc_score(labels, tensor)
-            vals.append(roc_auc)
-            tags.append("roc-auc")
-        if "pr-auc" in metric:
-            precision, recall, thresholds = metrics.precision_recall_curve(labels, tensor)
-            pr_auc = metrics.auc(recall, precision)
-            vals.append(pr_auc)
-            tags.append("pr-auc")
-        if "pr-score" in metric:
-            pr_score = metrics.average_precision_score(labels, tensor)
-            vals.append(pr_score)
-            tags.append("pr-score")
-        if "mcc" in metric:
-            mcc_score = metrics.matthews_corrcoef(labels, tensor)
-            vals.append(mcc_score)
-            tags.append("mcc")
-        if "f1" in metric:
-            f1_score = metrics.f1_score(labels, tensor)
-            vals.append(f1_score)
-            tags.append("f1")
+        res_dict = OrderedDict()
 
-        for val, tag in zip(vals, tags):
+        for m in metric:
+            res_dict[m] = _get_classification_metrics(tensor, labels, m)
+
+        for tag, val in res_dict.items():
             results_fn(val, name=tag + "-" + name, tag=tag_name)
 
-        return vals, tags
+        return res_dict
 
     if use_sub_process:
         p = Process(target=__get_classification_metrics, kwargs=dict(tensor=tensor,
@@ -188,5 +201,39 @@ def get_classification_metrics(tensor, labels, name="", metric=("roc-auc", "pr-s
 
         except Exception as e:
             warnings.warn("Sth went wrong with calculating the classification metrics")
-            v, t = zip(*[(0.0, m) for m in metric])
-            return v, t
+            ret = {m: 0.0 for m in metric}
+            return ret
+
+
+def get_classification_metric(tensor, labels, metric="roc-auc", use_sub_process=False):
+    """
+   Calculates a metric given the predicted values and the given/correct labels.
+
+    Args:
+        tensor: Tensor with scores (e.g class probability )
+        labels: Labels of the samples to which the scores match
+        metric: List of metrics to calculate. Options are: roc-auc, pr-auc, pr-score, mcc, f1
+        use_sub_process: Use a sub process to do the processing, if true nothing is returned
+
+    Returns:
+        The metric value
+
+    """
+
+    if use_sub_process:
+        p = Process(target=_get_classification_metrics, kwargs=dict(tensor=tensor,
+                                                                    labels=labels,
+                                                                    metric=metric,
+                                                                    ))
+        atexit.register(p.terminate)
+        p.start()
+    else:
+        try:
+            return _get_classification_metrics(tensor=tensor,
+                                               labels=labels,
+                                               metric=metric,
+                                               )
+
+        except Exception as e:
+            warnings.warn("Sth went wrong with calculating the classification metrics")
+            return 0.0
