dc203d7c6cda6ccdf2f24e8da3b40c8f4e3d6f7b
David Zimmerer <d.zimmerer@dkfz-heidelberg.de>
2020-09-28 15:18:34 +0200

    Catch Exception if matplotlib can not be imported in __init__.py

--- trixi/__init__.py
+++ trixi/__init__.py
@@ -1,5 +1,9 @@
-use_agg = False
-import matplotlib
-if use_agg: matplotlib.use("Agg", warn=False)
+try:
+    use_agg = False
+    import matplotlib
 
+    if use_agg:
+        matplotlib.use("Agg", warn=False)
+except Exception:
+    print("Failed to import matplotlib")
 __version__ = "0.1.2.2"
